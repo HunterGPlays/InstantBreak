cfba18b4eb3266dee376bfe67a953eaf4541a3ea
HunterGPlays <huntergplays@gmail.com>
2016-02-19 00:37:52 +0000

    Blocked worlds and only break while holding
    For version 1.0.1

--- src/main/java/net/minelink/instantbreak/InstantBreak.java
+++ src/main/java/net/minelink/instantbreak/InstantBreak.java
@@ -1,6 +1,7 @@
 package net.minelink.instantbreak;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import net.minelink.instantbreak.hooks.AACHook;
@@ -21,12 +22,16 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class InstantBreak extends JavaPlugin implements Listener {
-    public static InstantBreak plugin; // Create listener variable
-    public final static Set<Material> materials = new HashSet<>();
+    public InstantBreak plugin;
+    public static final Set<Material> materials = new HashSet<>();
+    public final List<String> blockedWorlds = getConfig().getStringList("blocked-worlds");
+    public final boolean restrictionsEnabled = getConfig().getBoolean("only-allow-instantbreak-with.enabled", false);
+    public final List<String> restrictionsItems = getConfig().getStringList("only-allow-instantbreak-with.items");
+    public final boolean restrictionsDamageItem = getConfig().getBoolean("only-allow-instantbreak-with.damage-item");
 
     @Override
     public void onEnable() {
-        plugin = this; // Assign listener plugin to this class
+        plugin = this;
         saveDefaultConfig();
 
         for (String mat : getConfig().getStringList("materials")) {
@@ -37,6 +42,17 @@ public final class InstantBreak extends JavaPlugin implements Listener {
                 getLogger().severe("Unknown material: " + mat);
             }
         }
+        
+        if (restrictionsEnabled) {
+            for (String item : restrictionsItems) {
+                try {
+                    Material.valueOf(item);
+                } catch (IllegalArgumentException e) {
+                    getLogger().severe("Unknown item: " + item);
+                }
+            }
+        }
+        
 
         Bukkit.getPluginManager().registerEvents(this, this);
         if (Bukkit.getPluginManager().isPluginEnabled("AAC")) {
@@ -48,6 +64,13 @@ public final class InstantBreak extends JavaPlugin implements Listener {
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+        
+        for (String s : blockedWorlds) {
+            if (player.getWorld().getName().equals(s)) {
+                return;
+            }
+        }
+        
         if (player.getGameMode() == GameMode.CREATIVE) {
             return;
         }
@@ -59,12 +82,25 @@ public final class InstantBreak extends JavaPlugin implements Listener {
 
         Block block = event.getClickedBlock();
         if (materials.contains(block.getType())) {
-            BlockBreakEvent blockBreakEvent = new BlockBreakEvent(block, player);
-            Bukkit.getPluginManager().callEvent(blockBreakEvent);
-            if (!blockBreakEvent.isCancelled()) {
-                block.getWorld().playEffect(block.getLocation(), Effect.STEP_SOUND, block.getType(), 16);
-                block.breakNaturally();
+            if (restrictionsEnabled) {
+                for (String item : restrictionsItems) {
+                    if (player.getItemInHand().getType() == Material.valueOf(item)) {
+                        breakBlock(block, player);
+                        return;
+                    }
+                }
+            } else { 
+                breakBlock(block, player);
             }
+        }
+    }
+    
+    public void breakBlock(Block block, Player player) {
+        BlockBreakEvent blockBreakEvent = new BlockBreakEvent(block, player);
+        Bukkit.getPluginManager().callEvent(blockBreakEvent);
+        if (!blockBreakEvent.isCancelled()) {
+            block.getWorld().playEffect(block.getLocation(), Effect.STEP_SOUND, block.getType(), 16);
+            block.breakNaturally();
         }
     }
 }
